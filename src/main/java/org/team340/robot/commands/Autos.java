42584a7f0d90d06e89194c2606dd8d9e6621cce9
caleb <calebjasonwallace@gmail.com>
2024-03-09 20:50:45 -0500

    Worked on FourPieceFar auto

--- src/main/java/org/team340/robot/commands/Autos.java
+++ src/main/java/org/team340/robot/commands/Autos.java
@@ -77,17 +77,36 @@ public class Autos {
         return parallel(
             pivot.targetDistance(swerve::getSpeakerDistance),
             sequence(
-                deadline(swerve.followTrajectory(traj.get(0), -1.0, -1.0, true), intake.downPosition()),
-                deadline(sequence(waitSeconds(1.15), feeder.shoot().withTimeout(0.75)), swerve.driveSpeaker()),
-                deadline(swerve.followTrajectory(traj.get(1)), Routines.intake()),
-                deadline(swerve.followTrajectory(traj.get(2)), Routines.intake().withTimeout(0.3)),
-                deadline(sequence(waitSeconds(0.25), feeder.shoot().withTimeout(0.75)), swerve.driveSpeaker()),
-                deadline(swerve.followTrajectory(traj.get(3)), Routines.intake()),
-                deadline(swerve.followTrajectory(traj.get(4)), Routines.intake().withTimeout(0.3)),
-                deadline(sequence(waitSeconds(0.25), feeder.shoot().withTimeout(0.75)), swerve.driveSpeaker()),
-                deadline(swerve.followTrajectory(traj.get(5)), Routines.intake()),
-                deadline(swerve.followTrajectory(traj.get(6)), Routines.intake().withTimeout(0.3)),
-                deadline(sequence(waitSeconds(0.25), feeder.shoot().withTimeout(0.6)), swerve.driveSpeaker())
+                deadline(
+                    // This goes to the first shooting position.
+                    swerve.followTrajectory(traj.get(0)),
+                    intake.downPosition()
+                ),
+                swerve.driveSpeaker().withTimeout(1.0),
+                feeder.shoot().withTimeout(0.1),
+                deadline(
+                    // this goes to pick up the first note.
+                    swerve.followTrajectory(traj.get(1)),
+                    Routines.intake()
+                ),
+                // This drives to the shooting position for the first note.
+                swerve.followTrajectory(traj.get(2)),
+                swerve.driveSpeaker().withTimeout(0.4),
+                feeder.shoot().withTimeout(0.1),
+                deadline(
+                    // This drives to the second note.
+                    swerve.followTrajectory(traj.get(3)),
+                    Routines.intake()
+                ),
+                deadline(
+                    // This goes to the third note.
+                    swerve.followTrajectory(traj.get(4), 0.8, -1),
+                    sequence(waitSeconds(1.3), feeder.shoot().withTimeout(0.4), Routines.intake())
+                ),
+                // This goes to the end position.
+                swerve.followTrajectory(traj.get(5)),
+                swerve.driveSpeaker().withTimeout(1.0),
+                feeder.shoot()
             )
         );
     }
