586524c7b9e51ac4107b24658cd4f9758a831e2b
Bryce Roethel <br88c@br88c.dev>
2024-04-26 20:07:04 -0400

    Fix dashboard default URI

--- dashboard/src/constants.ts
+++ dashboard/src/constants.ts
@@ -1,4 +1,4 @@
 // Default NT URI.
-// export const DEFAULT_URI = `10.3.40.2`;
+export const DEFAULT_URI = `10.3.40.2`;
 // export const DEFAULT_URI = `roborio-340-frc.local`;
-export const DEFAULT_URI = `localhost`; // Use for simulation
+// export const DEFAULT_URI = `localhost`; // Use for simulation
